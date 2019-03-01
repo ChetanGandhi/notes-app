dfa3166e16f920bee77bcec019323329c7e08bd0
Chetan Gandhi <projectchetan07@gmail.com>
2019-03-02 01:20:24 +0530

    Build process
    - Addede typescript config file.
    - Added webpack as build tool.
    - Updated webpack config to generate css from scss files.
    - Updated webpack config to auto generate index.html files.

---
+++ angular/htmlTemplate.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "Notes",
+    base: "/",
+    template: "./src/core/index.html",
+    chunksSortMode: "dependency",
+    meta: {
+        viewport: "width=device-width, initial-scale=1.0",
+        "Content-Security-Policy": { "http-equiv": "X-UA-Compatible", content: "ie=edge" }
+    }
+};
---
+++ angular/webpack.config.js
@@ -0,0 +1,88 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dist = path.join(__dirname, "../dist", "notes-angular");
+
+const htmlWebpackPluginConfig = require("./htmlTemplate.config");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        polyfills: "./src/core/polyfills.ts",
+        app: "./src/core/index.ts",
+        style: "./src/resources/scss/index.scss"
+    },
+    output: {
+        path: dist,
+        filename: "[name].js"
+    },
+    optimization: {
+        runtimeChunk: "single",
+        splitChunks: {
+            chunks: "all",
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `vendor.${packageName.replace("@", "")}`;
+                    }
+                }
+            }
+        }
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.ts$/,
+                loader: ["ts-loader", "angular2-template-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(/\@angular(\\|\/)core(\\|\/)fesm5/, path.resolve("src"), {}),
+        new webpack.DefinePlugin({
+            ENV_PRODUCTION: false
+        }),
+        // new CopyPlugin([{ from: "./src/core/index.html", toType: "file" }]),
+        new MiniCssExtractPlugin({
+            filename: "resources/css/[name].css",
+            chunkFilename: "resources/css/[id].css"
+        }),
+        new HtmlWebpackPlugin(htmlWebpackPluginConfig)
+    ]
+};
